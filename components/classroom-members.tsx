bca864d53769da15764bebf7e2ef27a42bb46cec
Paige Tinyow <paigetinyow@paiges-new-macbook-pro.local>
2025-03-16 14:09:42 +1100

    removing permissions

--- components/classroom-members.tsx
+++ components/classroom-members.tsx
@@ -231,18 +231,6 @@ export function ClassroomMembers({ classroomId, isOwner = false }) {
                         />
                       </div>
                       <div className="space-y-2">
-                        <Label htmlFor="role">Role</Label>
-                        <Select value={inviteRole} onValueChange={setInviteRole}>
-                          <SelectTrigger id="role">
-                            <SelectValue placeholder="Select role" />
-                          </SelectTrigger>
-                          <SelectContent>
-                            <SelectItem value="student">Student</SelectItem>
-                            <SelectItem value="teacher">Teacher</SelectItem>
-                          </SelectContent>
-                        </Select>
-                      </div>
-                      <div className="space-y-2">
                         <Label>Join Code</Label>
                         <div className="flex">
                           <Input value={joinCode} readOnly className="rounded-r-none" />
